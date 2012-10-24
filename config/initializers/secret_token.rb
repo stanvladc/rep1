2fdcee5fde009aad20d31dae1f4ef8afaf3cae31
razvan.bigiu <razvanbigiu@student.(none)>
2012-10-24 18:10:18 +0300

    al doilea commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Svladp1::Application.config.secret_token = '424c835a8964ced9ee541848143709d2d72dfe9a361483ff60c1f3626c4c9a300bec4798647d940925a7a1e81950d5d3157b454b409eed2d7b4b2939a6533a33'
